f9d82a424949ef8190805443e6dd7fe3497ba2c2
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2026-01-25 06:26:25 +0100

    initial commit

---
+++ src/main/java/com/gmail/ykomarnytskyi2022/CartaLingua/CartaLinguaApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.ykomarnytskyi2022.CartaLingua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaLinguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaLinguaApplication.class, args);
+	}
+
+}
