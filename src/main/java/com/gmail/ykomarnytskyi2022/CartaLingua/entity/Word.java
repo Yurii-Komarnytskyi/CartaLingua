14e87604ab59effd9e95736ec636edff8bf74d46
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2026-01-31 06:50:54 +0100

    Added canonical constructor + equals & hashcode to Word entity

--- src/main/java/com/gmail/ykomarnytskyi2022/CartaLingua/entity/Word.java
+++ src/main/java/com/gmail/ykomarnytskyi2022/CartaLingua/entity/Word.java
@@ -2,7 +2,9 @@ package com.gmail.ykomarnytskyi2022.CartaLingua.entity;
 
 import com.gmail.ykomarnytskyi2022.CartaLingua.enumeration.SupportedLanguages;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -10,14 +12,23 @@ import java.util.UUID;
 public class Word {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @OneToMany(mappedBy = "word")
     private UUID id;
 
+    @NotNull
     private String value;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private SupportedLanguages language;
 
     public Word() {
+    }
+
+    public Word(UUID id, String value, SupportedLanguages language) {
+        this.id = id;
+        this.value = value;
+        this.language = language;
     }
 
     public UUID getId() {
@@ -38,5 +49,25 @@ public class Word {
 
     public void setLanguage(SupportedLanguages language) {
         this.language = language;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Word word)) return false;
+        return Objects.equals(id, word.id) && Objects.equals(value, word.value) && language == word.language;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value, language);
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "id=" + id +
+                ", value='" + value + '\'' +
+                ", language=" + language +
+                '}';
     }
 }
