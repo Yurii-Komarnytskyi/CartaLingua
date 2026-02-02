16c7314c08dbfe6f2ed79656c4aec068096bf625
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2026-02-02 06:01:22 +0100

    Added @Repository onto FlashCardRepo

--- src/main/java/com/gmail/ykomarnytskyi2022/CartaLingua/repository/FlashCardRepo.java
+++ src/main/java/com/gmail/ykomarnytskyi2022/CartaLingua/repository/FlashCardRepo.java
@@ -2,8 +2,10 @@ package com.gmail.ykomarnytskyi2022.CartaLingua.repository;
 
 import com.gmail.ykomarnytskyi2022.CartaLingua.entity.FlashCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface FlashCardRepo extends JpaRepository<FlashCard, UUID> {
 }
