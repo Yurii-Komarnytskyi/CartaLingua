256df4a7770629e500ab54d00b8ebfd942fdc59c
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2026-02-27 21:00:56 +0100

    Fix, replacing @Min, @Max on String fields of the Dto records with @Size(max, min)

--- src/main/java/com/gmail/ykomarnytskyi2022/CartaLingua/dto/CreateWordDto.java
+++ src/main/java/com/gmail/ykomarnytskyi2022/CartaLingua/dto/CreateWordDto.java
@@ -1,11 +1,8 @@
 package com.gmail.ykomarnytskyi2022.CartaLingua.dto;
 
 import com.gmail.ykomarnytskyi2022.CartaLingua.enumeration.SupportedLanguages;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
-public record CreateWordDto (@NotNull @NotBlank @Min(value = 1) @Max(value = 100) String value,
+public record CreateWordDto (@NotNull @NotBlank @Size(min = 1, max = 100) String value,
                              @NotNull SupportedLanguages language){
 }
--- src/main/java/com/gmail/ykomarnytskyi2022/CartaLingua/dto/WordDto.java
+++ src/main/java/com/gmail/ykomarnytskyi2022/CartaLingua/dto/WordDto.java
@@ -1,14 +1,11 @@
 package com.gmail.ykomarnytskyi2022.CartaLingua.dto;
 
 import com.gmail.ykomarnytskyi2022.CartaLingua.enumeration.SupportedLanguages;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 import java.util.UUID;
 
 public record WordDto(@NotNull UUID id,
-                      @NotNull @NotBlank @Min(value = 1) @Max(value = 100)String value,
+                      @NotNull @NotBlank @Size(min = 1, max = 100) String value,
                       @NotNull SupportedLanguages language) {
 }
