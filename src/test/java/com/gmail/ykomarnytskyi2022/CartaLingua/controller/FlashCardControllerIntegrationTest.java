823f8062b370ca1e81a3fa918172f0002c490a16
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2026-03-15 07:42:46 +0100

    Test : FlashCardControllerIntegrationTest, 400 bad request scenarios for create()

--- src/test/java/com/gmail/ykomarnytskyi2022/CartaLingua/controller/FlashCardControllerIntegrationTest.java
+++ src/test/java/com/gmail/ykomarnytskyi2022/CartaLingua/controller/FlashCardControllerIntegrationTest.java
@@ -2,7 +2,6 @@ package com.gmail.ykomarnytskyi2022.CartaLingua.controller;
 
 import com.gmail.ykomarnytskyi2022.CartaLingua.dto.CreateFlashCardDto;
 import com.gmail.ykomarnytskyi2022.CartaLingua.dto.CreateWordDto;
-import com.gmail.ykomarnytskyi2022.CartaLingua.enumeration.SupportedLanguages;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,9 +14,11 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import tools.jackson.databind.json.JsonMapper;
 
-import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+import static com.gmail.ykomarnytskyi2022.CartaLingua.enumeration.SupportedLanguages.DUTCH;
+import static com.gmail.ykomarnytskyi2022.CartaLingua.enumeration.SupportedLanguages.ENGLISH;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -36,8 +37,11 @@ class FlashCardControllerIntegrationTest {
     private final RequestPostProcessor httpBasic = httpBasic("user", "password");
     private final String FLASHCARD_API = "http://localhost:8080/api/flashcard/";
     private final String APPLICATION_JSON = "application/json";
-    private final CreateFlashCardDto createDto = new CreateFlashCardDto(new CreateWordDto("goedkoop", SupportedLanguages.DUTCH),
-            "cheap", Optional.of("/tʃiːp/"), SupportedLanguages.ENGLISH);
+    private final String CHEAP = "cheap";
+    private final String CHEAP_TRANSCRIPTION = "/tʃiːp/";
+    private final String GOEDKOOP = "goedkoop";
+    private final CreateFlashCardDto createDto = new CreateFlashCardDto(new CreateWordDto(GOEDKOOP, DUTCH),
+            CHEAP, Optional.of(CHEAP_TRANSCRIPTION), ENGLISH);
 
     @Container
     static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18");
@@ -74,24 +78,58 @@ class FlashCardControllerIntegrationTest {
     }
 
     @Test
-    @DisplayName("create happy path")
+    @DisplayName("create() happy path + dto with nullable transcription ")
     void create() throws Exception {
+        var dtos = List.of(createDto, new CreateFlashCardDto(createDto.createWordDto(), CHEAP, null, ENGLISH));
+        for(var  dto : dtos) {
+            mockMvc.perform(
+                            post(FLASHCARD_API + "create")
+                                    .contentType(APPLICATION_JSON)
+                                    .content(jsonMapper.writeValueAsString(dto))
+                                    .with(httpBasic))
+                    .andExpectAll(
+                            status().isCreated(),
+                            jsonPath("$.id").exists(),
+                            jsonPath("$.wordDto.id").exists(),
+                            jsonPath("$.wordDto.value").value(createDto.createWordDto().value()),
+                            jsonPath("$.wordDto.language").value(createDto.createWordDto().language().toString()),
+                            jsonPath("$.translation").value(createDto.translation()),
+                            jsonPath("$.transcription").value(createDto.transcription().orElse("none")),
+                            jsonPath("$.creationDate").exists(),
+                            jsonPath("$.userBaseLanguage").value(createDto.userBaseLanguage().toString())
+                    );
+        }
+    }
+
+    @Test
+    @DisplayName("create() no createDto")
+    void createNoDto() throws Exception {
         mockMvc.perform(
-                post(FLASHCARD_API + "create")
+                        post(FLASHCARD_API + "create")
                                 .contentType(APPLICATION_JSON)
-                                .content(jsonMapper.writeValueAsString(createDto))
+                                .content("jsonMapper.writeValueAsString(createDto)")
                                 .with(httpBasic))
                 .andExpectAll(
-                        status().isCreated(),
-                        jsonPath("$.id").exists(),
-                        jsonPath("$.wordDto.id").exists(),
-                        jsonPath("$.wordDto.value").value(createDto.createWordDto().value()),
-                        jsonPath("$.wordDto.language").value(createDto.createWordDto().language().toString()),
-                        jsonPath("$.translation").value(createDto.translation()),
-                        jsonPath("$.transcription").value(createDto.transcription().orElse("none")),
-                        jsonPath("$.creationDate").value(LocalDate.now().toString()),
-                        jsonPath("$.userBaseLanguage").value(createDto.userBaseLanguage().toString())
+                        status().isBadRequest()
                 );
+    }
+
+    @Test
+    @DisplayName("create() malformed Dto")
+    void createMalformed() throws Exception {
+        var malformedDtos = List.of(
+                new CreateFlashCardDto(null, CHEAP, Optional.of(CHEAP_TRANSCRIPTION), ENGLISH),
+                new CreateFlashCardDto(createDto.createWordDto(), null, Optional.of(CHEAP_TRANSCRIPTION), ENGLISH),
+                new CreateFlashCardDto(createDto.createWordDto(), CHEAP, Optional.of(CHEAP_TRANSCRIPTION), null)
+        );
+        for(var dto : malformedDtos) {
+            mockMvc.perform(
+                            post(FLASHCARD_API + "create")
+                                    .contentType(APPLICATION_JSON)
+                                    .content(jsonMapper.writeValueAsString(dto))
+                                    .with(httpBasic))
+                    .andExpect(status().isBadRequest());
+        }
     }
 
     @Test
